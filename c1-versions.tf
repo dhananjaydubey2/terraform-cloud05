f780620cfd19b9ae9aaff35c116412b06636bf8b
dhananjaydubey1 <dhananjay.dubey2019@gmail.com>
2025-10-05 04:54:02 +0000

    fist-commit

---
+++ c1-versions.tf
@@ -0,0 +1,21 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+
+
+  }
+
+}
+
+
+# Provider Block
+provider "aws" {
+  #region = "ap-south-1"
+  region  = var.aws_region
+}
+
